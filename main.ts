c173425aef18214d58b52ab68e7e10e3921e5422
Rakhshan174 <160410026+Rakhshan174@users.noreply.github.com>
2024-04-23 06:13:56 -0700

    Q 15 TS/JS for 45 assignment

---
+++ main.ts
@@ -0,0 +1,20 @@
+//Changing Guest List: You just hesrd one of your guest can't make the dinner.so you need to send out a new
+// set of invitations. you'll have to think of someone else to invite.
+
+// (a) Start with your program from Exercise 14.Add a print statment at the end of your program stating the name 
+//of the guest who's can't make it
+
+// (b)Modify your list, replacing the name of the guest who can't make the name of the new person you are inviing.
+
+//(c) Print a second set of invitation  messages, one for each person who is still in your list.
+
+let guestArr : string  [] = ["Ali", "Wajahat", "Muzammil", "Haseeb"];
+let canNotAttend : string = "Muzammil";
+
+// console.log(canNotAttend + " " "can not attend the dinner.")
+
+let newGuest : string = "Hamza"
+guestArr [guestArr.indexOf(canNotAttend)] = newGuest;
+//  console.log(guestArr)
+
+guestArr.map((items) => (console.log(`Dear ${items}, you are invited to the dinner`)))
